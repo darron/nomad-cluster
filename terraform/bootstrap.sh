bf6da49f347d50150c38e56bfadb3c03cc849bc5
Darron Froese <darron@froese.org>
2015-11-19 18:05:51 -0700

    Better - but it still doesn't work.

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -3,8 +3,10 @@ sudo service nomad stop
 IP_ADDRESS=$(ifconfig eth0 | grep "inet addr" | cut --delimiter=":" -f 2 | cut --delimiter=" " -f 1)
 cat > /tmp/server.hcl <<EOF
 log_level = "DEBUG"
-bind_addr = "$IP_ADDRESS"
+enable_debug = true
+bind_addr = "0.0.0.0"
 advertise {
+  http = "$IP_ADDRESS:4646"
   rpc = "$IP_ADDRESS:4647"
   serf = "$IP_ADDRESS:4648"
 }
@@ -19,7 +21,28 @@ OPTIONS=""
 LOGFILE="/var/log/nomad/nomad.log"
 NOMAD_ADDR="http://$IP_ADDRESS:4646"
 EOF
+cat > /tmp/nomad.init <<EOF
+description "Nomad - A Distributed, Highly Available, Datacenter-Aware Scheduler"
+
+emits nomad-up
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+script
+  if [ -f /etc/default/nomad ]; then
+    . /etc/default/nomad
+  fi
+  export NOMAD_ADDR="http://$IP_ADDRESS:4646"
+  exec /usr/local/bin/nomad agent -config /etc/nomad.d >> /var/log/nomad/nomad.log
+end script
+
+post-start exec initctl emit nomad-up
+
+kill signal INT
+EOF
 sudo mv -f /tmp/server.hcl /etc/nomad.d/server.hcl
 sudo mv -f /tmp/nomad.default /etc/default/nomad
+sudo mv -f /tmp/nomad.init /etc/init/nomad.conf
 sudo rm -rf /var/lib/nomad/*
 sudo service nomad start
